eb36715327286add58478f2306b314ff489c9866
Mahir Musayev <146106439+musayevmahir68@users.noreply.github.com>
2024-05-07 00:48:29 +0400

    pronia mvs

---
+++ Pronia/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pronia.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult>  Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Contact()
+        {
+            return View();
+        }
+        public async Task<IActionResult> About()
+        {
+            return View();
+        }
+    }
+}
